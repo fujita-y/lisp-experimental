78057f775d00942b95d0f557881fe32efca1899c
Yoshikatsu Fujita <yfujita@twitter.com>
2013-03-27 18:09:24 +0900

    add foo

--- src/Main.scala
+++ src/Main.scala
@@ -1,6 +1,11 @@
-
 object Main {
+
+  def foo(x: Array[String]) = x.foldLeft("") {
+    (a, b) => a + b
+  }
+
   def main(args: Array[String]) {
     System.out.println("Hello, Scala World!")
+    println("concat arguments = " + foo(args))
   }
 }
